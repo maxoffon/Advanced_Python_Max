9cc1c62286f20a7f98c0a9dad4ba225e9c17b7b3
Maxoffon <moks.makarov@mail.ru>
2023-03-18 22:10:29 +0500

    fourth lesson accomplished, final change

--- mod4/ex5.py
+++ mod4/ex5.py
@@ -1,3 +1,4 @@
+import shlex
 import subprocess
 from flask import Flask, request
 
@@ -7,9 +8,15 @@ app = Flask(__name__)
 @app.route('/ps', methods=["GET"])
 def ps():
     args = request.args.getlist('arg')
-    print(args)
-    result = subprocess.run(args, capture_output=True).stdout.decode()
-    return f'Result: <pre>{result}</pre>'
+    args_clean = [shlex.quote(s) for s in args]
+    command_str = f"ps {' '.join(args_clean)}"
+    command = shlex.split(command_str)
+    result = subprocess.run(command, capture_output=True)
+
+    if result.returncode != 0:
+        return "Error", 500
+
+    return f'<pre>{result.stdout.decode()}</pre>'
 
 
 if __name__ == '__main__':
