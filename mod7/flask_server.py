3962307e07f77c0077039ca22bdc94138ea129f8
Maxoffon <moks.makarov@mail.ru>
2023-04-04 23:52:17 +0500

    7th lesson accomplished

--- mod7/flask_server.py
+++ mod7/flask_server.py
@@ -1,16 +1,21 @@
-from flask import Flask
+import json
+
+from flask import Flask, request
 
 app = Flask(__name__)
+logs = []
 
 
 @app.route('/runcode_post', methods=["POST"])
 def runcode_post():
-    return 'OK'
+    data = json.loads(request.get_data(as_text=True))
+    logs.append(' | '.join([data['msg'], data['levelname']]))
+    return 'ok'
 
 
 @app.route('/runcode_get', methods=["GET"])
 def runcode_get():
-    return 'OK'
+    return '<br>'.join(logs)
 
 
 if __name__ == '__main__':
